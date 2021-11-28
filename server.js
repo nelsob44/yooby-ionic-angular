c95dfd68809176b01bcbb609963d05b317136565
unknown <Nelson Oba>
2021-11-28 20:01:52 +0000

    deployment

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/www'));
 
 // Send all requests to index.html
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/www/index.html'));
 });
 
 // default Heroku PORT
